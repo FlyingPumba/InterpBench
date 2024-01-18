6ee384527e4a533ea64e653f400fec74492ae2cd
Ivan Arcuschin <iarcuschin@gmail.com>
2024-01-18 11:30:24 -0800

    Minor change to compile_benchmark_test.py

--- commands/compile_benchmark_test.py
+++ commands/compile_benchmark_test.py
@@ -1,13 +1,11 @@
 import unittest
 
 from commands.compile_benchmark import build_tracr_model
-from utils.attr_dict import AttrDict
 from utils.get_cases import get_cases
 
 
-class GetCasesTest(unittest.TestCase):
+class CompileBenchmarkTest(unittest.TestCase):
   def test_all_cases_compile_successfully(self):
-    # args = AttrDict({"indices": "2"})
     cases = get_cases(None)
     for case in cases:
       print(f"\nCompiling {case}")
