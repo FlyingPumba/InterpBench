31cd197e6bf149ec42f3ef71c8ffb183f05d4d31
tkwa <kwathomas0@gmail.com>
2024-01-31 03:26:32 +0000

    write correspondence class and __init__ method

--- examine_graph.py
+++ examine_graph.py
@@ -12,6 +12,7 @@ import numpy as np
 import torch
 import torch as t
 from typing import Callable
+from dataclasses import dataclass
 
 from benchmark.benchmark_case import BenchmarkCase
 from compression.residual_stream import compress, setup_compression_training_args_for_parser
@@ -19,7 +20,7 @@ from tracr.compiler import compiling
 from tracr.compiler.assemble import AssembledTransformerModel
 from tracr.compiler.compiling import TracrOutput
 from tracr.transformer.encoder import CategoricalEncoder
-from tracr.craft.transformers import MLP, MultiAttentionHead
+from tracr.craft.transformers import MLP, MultiAttentionHead, SeriesWithResiduals
 from tracr.craft import bases
 from utils.get_cases import get_cases
 from utils.hooked_tracr_transformer import HookedTracrTransformer
@@ -106,6 +107,46 @@ for block in tracr_output.craft_model.blocks:
     
 graph3 = tracr_output.graph
 craft_model3 = tracr_output.craft_model
+
+class TracrCorrespondence:
+  """
+  Stores a dictionary that takes tracr graph nodes to HookPoint nodes...
+  """
+  def __init__(self, graph: nx.DiGraph, craft_model: SeriesWithResiduals):
+    self.graph = graph
+    self.craft_model = craft_model
+
+    self._dict = dict()
+
+    for i, block in enumerate(tracr_output.craft_model.blocks):
+      if isinstance(block, MLP):
+          assert block.fst.output_space == block.snd.input_space
+          for direction in block.snd.output_space.basis:
+            self._dict[direction] = (i, "mlp", None)
+      elif isinstance(block, MultiAttentionHead):
+          for j, sb in enumerate(block.sub_blocks):
+            for direction in sb.w_ov.output_space.basis:
+              self._dict[direction] = (i, "attn", j)
+      else:
+        raise ValueError(f"Unknown block type {type(block)}")
+    
+    """
+    This is not necessary for now, because node names and direction names are equal
+    """
+    # for name, node in graph.nodes.items():
+    #   if name in ["indices", "tokens"]: continue
+    #   if "OUTPUT_BASIS" in node:
+    #     for direction in node["OUTPUT_BASIS"]:
+    #       self._dict[direction] = self._unit_output_bases[direction]
+
+  def __getitem__(self, key):
+    return self._dict[key]
+  
+  def __repr__(self) -> str:
+    return f"TracrCorrespondence(\n{self._dict}\n)"
+
+corr = TracrCorrespondence(graph3, craft_model3)
+print(corr)
 
 # %%
 
