e233dd05ea25950be2d99e45befb0ef27a697775
Ivan Arcuschin <iarcuschin@gmail.com>
2024-02-12 13:51:28 -0800

    Add assertion to make sure that clean and corrupted inputs are never exactly the same in resampling ablation loss

--- utils/resampling_ablation_loss.py
+++ utils/resampling_ablation_loss.py
@@ -17,12 +17,17 @@ def get_resampling_ablation_loss(
     hypothesis_model: HookedTransformer,
     intervention_filters: List[str] = ["hook_embed", "hook_attn_out", "hook_mlp_out"]
 ) -> Float[Tensor, ""]:
-  # we assume that both models have the same architecture. Otherwise, the comparison is flawed since they have diffenret
+  # we assume that both models have the same architecture. Otherwise, the comparison is flawed since they have different
   # intervention points.
   assert base_model.cfg.n_layers == hypothesis_model.cfg.n_layers
   assert base_model.cfg.n_heads == hypothesis_model.cfg.n_heads
   assert base_model.cfg.n_ctx == hypothesis_model.cfg.n_ctx
   assert base_model.cfg.d_vocab == hypothesis_model.cfg.d_vocab
+
+  # assert that clean and corrupted inputs are not exactly the same, otherwise the comparison is flawed.
+  all_equal = all(clean_input == corrupted_input
+                  for clean_input, corrupted_input in zip(clean_inputs, corrupted_inputs))
+  assert not all_equal, "clean and corrupted inputs are exactly the same. This is not a valid comparison."
 
   # first, we run the corrupted inputs on both models and save the activation caches.
   _, base_model_corrupted_cache = base_model.run_with_cache(corrupted_inputs)
