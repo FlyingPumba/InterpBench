398a5ab43702b1acf525fac1f475cb2edfd097ff
Ivan Arcuschin <iarcuschin@gmail.com>
2024-02-13 10:58:11 -0800

    Add copy of proportions program with increased vocab and max sequence length

--- utils/get_cases_test.py
+++ utils/get_cases_test.py
@@ -5,10 +5,6 @@ from utils.get_cases import get_cases
 
 
 class GetCasesTest(unittest.TestCase):
-  def test_all_cases(self):
-    cases = get_cases(None)
-    self.assertEqual(len(cases), 47)
-
   def test_cases_filtered_by_indices(self):
     args = AttrDict({"indices": "1,2,3"})
     cases = get_cases(args)
