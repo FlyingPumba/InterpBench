9694bad8e4aad16204b86fd4e50d0b8f2f304b70
Ivan Arcuschin <iarcuschin@gmail.com>
2024-03-04 21:00:07 -0800

    Skip tests for resample ablation loss

--- circuits_benchmark/metrics/resampling_ablation_loss/resample_ablation_loss_test.py
+++ circuits_benchmark/metrics/resampling_ablation_loss/resample_ablation_loss_test.py
@@ -409,7 +409,7 @@ class ResampleAblationLossTest(unittest.TestCase):
         f"{output_dir}/resample-ablation-loss-variance-re-init-no-op-matrix-random-data-and-interventions-{idx_plot + 1}.png")
 
   def test_variance_for_natural_transformer(self):
-    # self.skipTest("This test takes a long time to run, so it is skipped by default.")
+    self.skipTest("This test takes a long time to run, so it is skipped by default.")
     output_dir = get_default_output_dir()
     case = Case3()
     original_tracr_model: HookedTracrTransformer = case.get_tl_model()
@@ -497,7 +497,7 @@ class ResampleAblationLossTest(unittest.TestCase):
         f"{output_dir}/resample-ablation-loss-var-exp-for-natural-compression-{idx_plot + 1}.png")
 
   def test_variance_for_linear_transformer(self):
-    # self.skipTest("This test takes a long time to run, so it is skipped by default.")
+    self.skipTest("This test takes a long time to run, so it is skipped by default.")
     output_dir = get_default_output_dir()
     case = Case3()
     original_tracr_model: HookedTracrTransformer = case.get_tl_model()
@@ -587,7 +587,7 @@ class ResampleAblationLossTest(unittest.TestCase):
         f"{output_dir}/resample-ablation-loss-var-exp-for-linear-compression-{idx_plot + 1}.png")
 
   def test_variance_for_non_linear_transformer(self):
-    # self.skipTest("This test takes a long time to run, so it is skipped by default.")
+    self.skipTest("This test takes a long time to run, so it is skipped by default.")
     output_dir = get_default_output_dir()
     case = Case3()
     original_tracr_model: HookedTracrTransformer = case.get_tl_model()
