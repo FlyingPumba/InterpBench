8349c6a2c8777732b462652e4b3000180278b058
Ivan Arcuschin <iarcuschin@gmail.com>
2024-03-08 12:03:14 -0800

    Fix params not being updated for linear compressed transformer

--- circuits_benchmark/metrics/sparsity.py
+++ circuits_benchmark/metrics/sparsity.py
@@ -1,7 +1,15 @@
+from circuits_benchmark.training.compression.linear_compressed_tracr_transformer import LinearCompressedTracrTransformer
+
+
 def get_zero_weights_pct(model, atol=1e-8):
   """Returns the percentage of weights that are zero (or very close to zero given the atol)."""
   weights= []
-  for param in model.parameters():
+  if isinstance(model, LinearCompressedTracrTransformer):
+    params = list(model.folded_parameters())
+  else:
+    params = list(model.parameters())
+
+  for param in params:
     weights.extend(param.flatten().tolist())
 
   non_zero_weights = len([w for w in weights if abs(w) > atol])
