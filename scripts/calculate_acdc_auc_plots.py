dadf733d7cc8cecb9a3f5b24257d50ea787b6270
Ivan Arcuschin <iarcuschin@gmail.com>
2024-03-11 00:58:57 -0700

    Fix sort of fpr-tpr in calculate_acdc_auc_plots.py script

--- scripts/calculate_acdc_auc_plots.py
+++ scripts/calculate_acdc_auc_plots.py
@@ -26,10 +26,19 @@ if __name__ == "__main__":
     elif "natural-compression" in run_name:
       method = "natural"
     else:
-      raise ValueError(f"Unknown method in run {run_name}")
+      method = "tracr"
+
+    if "edges_fpr" not in run.summary or "edges_tpr" not in run.summary:
+      print(f"Skipping run {run.name} because it does not have the required metrics.")
+      continue
+
+    if "threshold" not in run.config:
+      print(f"Skipping run {run.name} because it does not have the threshold in the config.")
+      continue
 
     fpr = run.summary["edges_fpr"]
     tpr = run.summary["edges_tpr"]
+    threshold = run.config["threshold"]
 
     if case not in data_by_case:
       data_by_case[case] = {}
@@ -37,23 +46,31 @@ if __name__ == "__main__":
     if method not in data_by_case[case]:
       data_by_case[case][method] = {
         "fpr": [],
-        "tpr": []
+        "tpr": [],
+        "thresholds": []
       }
 
     data_by_case[case][method]["fpr"].append(fpr)
     data_by_case[case][method]["tpr"].append(tpr)
+    data_by_case[case][method]["thresholds"].append(threshold)
 
   # Now we have the data, we can calculate the AUC and plot the ROC curve.
-  for case, data_by_methods in data_by_case.items():
+  cases = sorted(list(data_by_case.keys()))
+  for case in cases:
+    data_by_methods = data_by_case[case]
+
     # reset plot
     plt.clf()
 
     # create new figure and axis
     fig, ax = plt.subplots()
 
-    for method, data in data_by_methods.items():
-      # zip data together and sort it by fpr
-      data = sorted(zip(data["fpr"], data["tpr"]))
+    methods = sorted(list(data_by_methods.keys())) # ['linear', 'natural', 'non-linear', 'tracr']
+    for method in methods:
+      data = data_by_methods[method]
+
+      # zip data together and sort it by threshold
+      data = sorted(zip(data["fpr"], data["tpr"], data["thresholds"]), key=lambda x: x[2], reverse=True)
 
       # split again into fpr and tpr
       fpr = [x[0] for x in data]
