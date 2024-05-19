51e3a2385440c4e047321a16a8c69204fb724418
cybershiptrooper <cybershiptrooper@gmail.com>
2024-05-19 03:46:33 +0100

    fix iit training

--- circuits_benchmark/utils/iit/iit_hl_model.py
+++ circuits_benchmark/utils/iit/iit_hl_model.py
@@ -1,10 +1,12 @@
 from typing import Any
 from transformer_lens import HookedTransformer
+import torch
 
 
 def make_iit_hl_model(hl_model):
     class IITHLModel:
         """A wrapper class to make tracr models compatible with IITModelPair"""
+
         def __init__(self, hl_model: HookedTransformer):
             self.hl_model = hl_model
 
@@ -12,29 +14,37 @@ def make_iit_hl_model(hl_model):
             if hasattr(self.hl_model, name):
                 return getattr(self.hl_model, name)
             else:
-                raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")
+                raise AttributeError(
+                    f"'{type(self).__name__}' object has no attribute '{name}'"
+                )
+
+        def create_hl_output(self, y):
+            if self.hl_model.is_categorical():
+                # convert to one-hot
+                return torch.nn.functional.one_hot(
+                    y.argmax(dim=-1), num_classes=y.shape[-1]
+                ).float()
+            return y
 
         def forward(self, input):
             x, _, _ = input
             out = self.hl_model(x)
-            if self.hl_model.is_categorical():
-                return out.argmax(dim=1)
-            return out
+            return self.create_hl_output(out)
 
         def run_with_hooks(self, input, *args, **kwargs):
             x, _, _ = input
             out = self.hl_model.run_with_hooks(x, *args, **kwargs)
-            if self.hl_model.is_categorical():
-                return out.argmax(dim=1)
-            return out
+            # if self.hl_model.is_categorical():
+            #     return out.argmax(dim=1)
+            return self.create_hl_output(out)
 
         def run_with_cache(self, input):
             x, _, _ = input
             out, cache = self.hl_model.run_with_cache(x)
-            if self.hl_model.is_categorical():
-                return out.argmax(dim=1), cache
-            return out, cache
-        
+            # if self.hl_model.is_categorical():
+            #     return out.argmax(dim=1), cache
+            return self.create_hl_output(out), cache
+
         def __call__(self, *args: Any, **kwds: Any) -> Any:
             return self.forward(*args, **kwds)
 
