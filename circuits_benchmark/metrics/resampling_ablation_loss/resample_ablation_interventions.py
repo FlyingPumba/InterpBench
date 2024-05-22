172d84052b7c0b1575a00981f239ef48913f9bbc
Ivan Arcuschin <iarcuschin@gmail.com>
2024-05-22 11:35:15 +0100

    Minor fix to rank selection for choosing intervention components

--- circuits_benchmark/metrics/resampling_ablation_loss/resample_ablation_interventions.py
+++ circuits_benchmark/metrics/resampling_ablation_loss/resample_ablation_interventions.py
@@ -52,28 +52,31 @@ def get_interventions(
   options.remove(InterventionType.NO_INTERVENTION)
 
   for _ in range(max_interventions):
-    # choose max_components_to_intervene (no replacement) out of the node_names_for_patching
+    components_to_intervene = random.randint(1, min(max_components, len(node_names_for_patching)))
+
     if effect_diffs_by_node is None:
-      node_names_to_intervene = random.sample(node_names_for_patching, max_components)
+      # choose components_to_intervene (no replacement) out of the node_names_for_patching
+      node_names_to_intervene = random.sample(node_names_for_patching, components_to_intervene)
     else:
       # perform Rank Selection based on effect_diffs_by_node (the largest the effect, the higher the probability)
       node_names_to_intervene = []
 
       # set the effect_diffs of the nodes that are not in effect_diffs_by_node to 1 (the maximum)
       effect_diffs_by_node = effect_diffs_by_node.copy()
-      for node in node_names_for_patching:
+      node_names_for_patching_in_this_intervention = node_names_for_patching[:]
+      for node in node_names_for_patching_in_this_intervention:
         if node not in effect_diffs_by_node:
           effect_diffs_by_node[node] = 1
 
-      for _ in range(max_components):
+      for _ in range(components_to_intervene):
         # Compute rank weights. If a node is not in effect_diffs_by_node, it is considered to have an effect diff of 1 (the maximum)
-        total_effect_diff = sum([effect_diffs_by_node[node] for node in node_names_for_patching])
-        rank_weights = [effect_diffs_by_node[node] / total_effect_diff for node in node_names_for_patching]
+        total_effect_diff = sum([effect_diffs_by_node[node] for node in node_names_for_patching_in_this_intervention])
+        rank_weights = [effect_diffs_by_node[node] / total_effect_diff for node in node_names_for_patching_in_this_intervention]
 
-        # pick max_components out of the node_names_for_patching
-        node = random.choices(node_names_for_patching, weights=rank_weights, k=1)[0]
+        # pick max_components out of the node_names_for_patching_in_this_intervention
+        node = random.choices(node_names_for_patching_in_this_intervention, weights=rank_weights, k=1)[0]
         node_names_to_intervene.append(node)
-        node_names_for_patching.remove(node)
+        node_names_for_patching_in_this_intervention.remove(node)
 
     # randomly choose the intervention type for each hook name
     intervention_types = [random.choice(options) for _ in range(len(node_names_to_intervene))]
