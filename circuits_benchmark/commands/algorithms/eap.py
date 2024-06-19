167ad9b1f1bdfd1177a6ea4e1f5a1c342b5799af
Ivan Arcuschin <iarcuschin@gmail.com>
2024-06-19 10:01:37 +0100

    Fix EAP algorithm

--- circuits_benchmark/commands/algorithms/eap.py
+++ circuits_benchmark/commands/algorithms/eap.py
@@ -1,7 +1,8 @@
 from argparse import Namespace, ArgumentParser
+from typing import List, Any
 
 import torch as t
-from auto_circuit.data import PromptDataLoader, PromptDataset
+from auto_circuit.data import PromptDataLoader, PromptDataset, PromptPairBatch
 from auto_circuit.prune_algos.edge_attribution_patching import edge_attribution_patching_prune_scores
 from auto_circuit.types import PruneScores
 from auto_circuit.utils.graph_utils import patchable_model
@@ -11,49 +12,92 @@ from jaxtyping import Float
 
 from acdc.TLACDCCorrespondence import TLACDCCorrespondence
 from circuits_benchmark.benchmark.benchmark_case import BenchmarkCase
+from circuits_benchmark.benchmark.vocabs import TRACR_BOS, TRACR_PAD
 from circuits_benchmark.commands.common_args import add_common_args
 from circuits_benchmark.transformers.acdc_circuit_builder import build_acdc_circuit
 from circuits_benchmark.transformers.circuit import Circuit
 from circuits_benchmark.transformers.circuit_node import CircuitNode
 from circuits_benchmark.utils.circuits_comparison import calculate_fpr_and_tpr
+from circuits_benchmark.utils.compare_tracr_output import replace_invalid_positions_in_expected_outputs
+from tracr.transformer.encoder import CategoricalEncoder
 
 
 class EAPRunner:
   def __init__(self, case: BenchmarkCase, args: Namespace):
     self.case = case
     self.args = args
+    self.data_size = args.data_size
     self.edge_count = args.top_k
 
   def run(self):
     tl_model = self.case.get_tl_model()
-    assert tl_model.is_categorical(), \
-      "EAP only works with categorical models for now, due to the way it computes the answer diff for loss."
 
     auto_circuit_model = patchable_model(
       tl_model,
       factorized=True,
-      slice_output="last_seq",
+      slice_output=None,
       separate_qkv=True,
       device=self.args.device,
     )
 
-    clean_dataset = self.case.get_clean_data(count=100)
-    corrupt_dataset = self.case.get_corrupted_data(count=100)
+    clean_dataset = self.case.get_clean_data(count=self.data_size)
+    corrupted_dataset = self.case.get_corrupted_data(count=self.data_size)
+
+    # remove from inputs the rows that have the same expected output
+    clean_raw_inputs = clean_dataset.get_inputs()
+    corrupted_raw_inputs = corrupted_dataset.get_inputs()
+    clean_expected_outputs = clean_dataset.get_correct_outputs()
+    corrupted_expected_outputs = corrupted_dataset.get_correct_outputs()
+
+    idxs_to_remove = []
+    for i in range(len(clean_expected_outputs)):
+      if clean_expected_outputs[i] == corrupted_expected_outputs[i]:
+        idxs_to_remove.append(i)
+
+    clean_inputs = [clean_raw_inputs[i] for i in range(len(clean_raw_inputs)) if i not in idxs_to_remove]
+    corrupted_inputs = [corrupted_raw_inputs[i] for i in range(len(corrupted_raw_inputs)) if i not in idxs_to_remove]
+
+    # Convert inputs to tensors using tracr encoder
+    clean_inputs = tl_model.map_tracr_input_to_tl_input(clean_inputs)
+    corrupted_inputs = tl_model.map_tracr_input_to_tl_input(corrupted_inputs)
+
+    # Use as correct and wrong answers the output of tracr model on the filtered inputs
+    with t.no_grad():
+      if tl_model.is_categorical():
+        clean_outputs = tl_model(clean_inputs).argmax(dim=-1).unsqueeze(dim=-1)
+        corrupted_outputs = tl_model(corrupted_inputs).argmax(dim=-1).unsqueeze(dim=-1)
+      else:
+        clean_outputs = tl_model(clean_inputs)
+        corrupted_outputs = tl_model(corrupted_inputs)
 
     dataset = PromptDataset(
-      tl_model.map_tracr_input_to_tl_input(clean_dataset.get_inputs()),
-      tl_model.map_tracr_input_to_tl_input(corrupt_dataset.get_inputs()),
-      tl_model(clean_dataset.get_inputs()).argmax(dim=-1),
-      tl_model(corrupt_dataset.get_inputs()).argmax(dim=-1),
+      clean_inputs,
+      corrupted_inputs,
+      clean_outputs,
+      corrupted_outputs,
     )
     train_loader = PromptDataLoader(dataset, seq_len=self.case.get_max_seq_len(), diverge_idx=0)
 
-    attribution_scores: PruneScores = edge_attribution_patching_prune_scores(
-      model=auto_circuit_model,
-      dataloader=train_loader,
-      official_edges=None,
-      answer_diff=tl_model.is_categorical(),
-    )
+    if tl_model.is_categorical():
+      # For categorical models we use as loss function the diff between the correct and wrong answers
+      attribution_scores: PruneScores = edge_attribution_patching_prune_scores(
+        model=auto_circuit_model,
+        dataloader=train_loader,
+        official_edges=None,
+        answer_diff=True,
+      )
+    else:
+      # Auto-circuit assumes that all models are categorical, so we need to provide a custom loss function for
+      # regression ones
+      def loss_fn(logits: t.Tensor, batch: PromptPairBatch) -> t.Tensor:
+        return -t.mean(logits - batch.wrong_answers)
+
+      attribution_scores: PruneScores = edge_attribution_patching_prune_scores(
+        model=auto_circuit_model,
+        dataloader=train_loader,
+        official_edges=None,
+        loss_fn=loss_fn,
+      )
 
     # find the threshold for the top-k edges and build circuit using that threshold
     threshold = prune_scores_threshold(attribution_scores, self.edge_count)
@@ -112,6 +156,7 @@ class EAPRunner:
     )
 
     parser.add_argument("--top-k", "-k", type=int, default=2, help="Number of edges to keep in the final circuit")
+    parser.add_argument("--data-size", type=int, default=1000, help="Number of samples to use")
 
   @classmethod
   def make_default_runner(cls, task: str):
