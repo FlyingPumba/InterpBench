7de4b97a5b399928013e02b975f6f230b82e6b8d
Ivan Arcuschin <iarcuschin@gmail.com>
2024-07-09 11:06:50 +0100

    Add tests

---
+++ tests/tracr_dataset_test.py
@@ -0,0 +1,12 @@
+import pytest
+
+from circuits_benchmark.benchmark.cases.case_3 import Case3
+from circuits_benchmark.benchmark.cases.case_9 import Case9
+
+
+class TestTracrDataset:
+
+  @pytest.mark.parametrize("case", [Case3(), Case9()])
+  def test_get_encoded_dataset(self, case):
+    data = case.get_clean_data()
+    assert len(data) == 10
