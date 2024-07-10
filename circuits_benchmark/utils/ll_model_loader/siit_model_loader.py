3577c7094972ebb36f8adfd5411694b34e5738b7
cybershiptrooper <cybershiptrooper@gmail.com>
2024-07-10 13:22:27 +0100

    fix SIIT model loader

--- circuits_benchmark/utils/ll_model_loader/siit_model_loader.py
+++ circuits_benchmark/utils/ll_model_loader/siit_model_loader.py
@@ -1,15 +1,15 @@
 import pickle
+import tempfile
 from typing import Optional, Tuple
 
 import torch
-from iit.utils.correspondence import Correspondence
 from transformer_lens import HookedTransformer
 
 from circuits_benchmark.benchmark.benchmark_case import BenchmarkCase
-from circuits_benchmark.transformers.hooked_tracr_transformer import HookedTracrTransformer
-from circuits_benchmark.utils.ll_model_loader.best_weights import get_best_weight
 from circuits_benchmark.utils.iit.wandb_loader import load_model_from_wandb
+from circuits_benchmark.utils.ll_model_loader.best_weights import get_best_weight
 from circuits_benchmark.utils.ll_model_loader.ll_model_loader import LLModelLoader
+from iit.utils.correspondence import Correspondence
 
 
 class SIITModelLoader(LLModelLoader):
@@ -41,6 +41,25 @@ class SIITModelLoader(LLModelLoader):
       same_size: bool = False,
       *args, **kwargs
   ) -> Tuple[Correspondence, HookedTransformer]:
+    assert self.load_from_wandb or output_dir is not None, ValueError("Either load from wandb or provide directory to load from")
+    if output_dir is None:
+      # make temporary directory
+      with tempfile.TemporaryDirectory() as output_dir:
+        print(f"Created temporary directory at {output_dir}")
+        return self.load_ll_model_and_correspondence(
+          device, output_dir, same_size=same_size, *args, **kwargs
+        )
+
+    if self.load_from_wandb:
+      try:
+        load_model_from_wandb(
+          self.case.get_name(), self.weights, output_dir, same_size=same_size
+        )
+      except FileNotFoundError:
+        raise FileNotFoundError(
+          f"Could not find SIIT model with weights {self.weights} for case {self.case.get_name()} in wandb"
+        )
+      
     try:
       ll_cfg = pickle.load(
         open(
@@ -53,16 +72,6 @@ class SIITModelLoader(LLModelLoader):
 
     ll_model = HookedTransformer(ll_cfg)
     hl_ll_corr = self.case.get_correspondence(*args, **kwargs)
-
-    if self.load_from_wandb:
-      try:
-        load_model_from_wandb(
-          self.case.get_name(), self.weights, output_dir, same_size=same_size
-        )
-      except FileNotFoundError:
-        raise FileNotFoundError(
-          f"Could not find SIIT model with weights {self.weights} for case {self.case.get_name()} in wandb"
-        )
 
     ll_model.load_state_dict(torch.load(
       f"{output_dir}/ll_models/{self.case.get_name()}/ll_model_{self.weights}.pth",
