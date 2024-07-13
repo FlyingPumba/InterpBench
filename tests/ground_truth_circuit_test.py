91c179767c445beacfb68da37b28187bae682dcd
cybershiptrooper <cybershiptrooper@gmail.com>
2024-07-13 01:31:22 +0100

    fix get_gt_circuit, move prepare_circuit_for_evaluation and make gt_circuit test run for all cases

--- tests/ground_truth_circuit_test.py
+++ tests/ground_truth_circuit_test.py
@@ -1,24 +1,28 @@
 from acdc.TLACDCCorrespondence import TLACDCCorrespondence
 
-from circuits_benchmark.benchmark.cases.case_7 import Case7
 from circuits_benchmark.utils.circuit.circuit_eval import build_from_acdc_correspondence
+from circuits_benchmark.utils.get_cases import get_cases
 from circuits_benchmark.utils.iit._acdc_utils import get_gt_circuit
+from circuits_benchmark.benchmark.cases.case_ioi import CaseIOI
 
 
 class TestGroundTruthCircuit:
-  def test_gt_circuit_for_case_7(self):
-    case = Case7()
 
-    full_corr = TLACDCCorrespondence.setup_from_model(case.get_ll_model())
-    full_circuit = build_from_acdc_correspondence(corr=full_corr)
+  def test_gt_circuit_for_all_cases(self):
+    cases = get_cases()
+    cases = [case for case in cases if not isinstance(case, CaseIOI)] # remove ioi cases
 
-    corr = case.get_correspondence()
+    for case in cases:
+      full_corr = TLACDCCorrespondence.setup_from_model(case.get_ll_model())
+      full_circuit = build_from_acdc_correspondence(corr=full_corr)
 
-    gt_circuit = get_gt_circuit(
-      hl_ll_corr=corr,
-      full_circuit=full_circuit,
-      n_heads=case.get_ll_model().cfg.n_heads,
-      case=case,
-    )
+      corr = case.get_correspondence()
 
-    assert len(gt_circuit.edges) > 0
+      gt_circuit = get_gt_circuit(
+        hl_ll_corr=corr,
+        full_circuit=full_circuit,
+        n_heads=case.get_ll_model().cfg.n_heads,
+        case=case,
+      )
+
+      assert len(gt_circuit.edges) > 0, f"Case {case} has no edges in gt_circuit"
