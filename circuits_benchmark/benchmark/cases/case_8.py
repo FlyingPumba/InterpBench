99182638b08da95498e6b049f097fe872b877c26
Ivan Arcuschin <iarcuschin@gmail.com>
2024-10-02 18:44:55 +0100

    Swap cases 8 and 90

--- circuits_benchmark/benchmark/cases/case_8.py
+++ circuits_benchmark/benchmark/cases/case_8.py
@@ -8,29 +8,17 @@ from circuits_benchmark.benchmark.tracr_benchmark_case import TracrBenchmarkCase
 
 class Case8(TracrBenchmarkCase):
     def get_program(self) -> rasp.SOp:
-        return make_token_replacer(rasp.tokens, "findme", "-")
+        return make_identity()
 
     def get_task_description(self) -> str:
-        return "Replaces a specific token with another one."
+        return "Identity"
 
     def get_vocab(self) -> Set:
-        vocab = vocabs.get_words_vocab()
-        vocab.add("findme")
-        vocab.add("-")
-        return vocab
+        return vocabs.get_int_numbers_vocab()
 
     def is_trivial(self) -> bool:
         return True
 
 
-def make_token_replacer(sop: rasp.SOp, target: str, replacement: str) -> rasp.SOp:
-    """
-    Returns a program that replaces a target token with a replacement token
-
-    Example usage:
-      replacer = make_token_replacer(rasp.tokens, "findme", "-")
-      replacer(["word1", "findme", "word3"])
-      >> ["word1", "-", "word3"]
-    """
-    replaced = rasp.Map(lambda x: replacement if x == target else x, sop)
-    return replaced
+def make_identity() -> rasp.SOp:
+    return rasp.Map(lambda x: x, rasp.tokens)
--- circuits_benchmark/benchmark/cases/case_90.py
+++ circuits_benchmark/benchmark/cases/case_90.py
@@ -8,17 +8,29 @@ from circuits_benchmark.benchmark.tracr_benchmark_case import TracrBenchmarkCase
 
 class Case90(TracrBenchmarkCase):
     def get_program(self) -> rasp.SOp:
-        return make_identity()
+        return make_token_replacer(rasp.tokens, "findme", "-")
 
     def get_task_description(self) -> str:
-        return "Identity"
+        return "Replaces a specific token with another one."
 
     def get_vocab(self) -> Set:
-        return vocabs.get_int_numbers_vocab()
+        vocab = vocabs.get_words_vocab()
+        vocab.add("findme")
+        vocab.add("-")
+        return vocab
 
     def is_trivial(self) -> bool:
         return True
 
 
-def make_identity() -> rasp.SOp:
-    return rasp.Map(lambda x: x, rasp.tokens)
+def make_token_replacer(sop: rasp.SOp, target: str, replacement: str) -> rasp.SOp:
+    """
+    Returns a program that replaces a target token with a replacement token
+
+    Example usage:
+      replacer = make_token_replacer(rasp.tokens, "findme", "-")
+      replacer(["word1", "findme", "word3"])
+      >> ["word1", "-", "word3"]
+    """
+    replaced = rasp.Map(lambda x: replacement if x == target else x, sop)
+    return replaced
